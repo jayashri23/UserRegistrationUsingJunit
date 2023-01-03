430b1264580df2a88b6e653ae37ce5f8f8179045
jayashri23 <jsjankar23@gmail.com>
2023-01-03 21:50:07 +0530

    Validation for Email id

--- src/com/junit/UserRegistrationTest.java
+++ src/com/junit/UserRegistrationTest.java
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Test;
 public class UserRegistrationTest {@Test
 public void givenFirstName_WhenProper_ShouldReturnTrue() {
     UserRegistration userRegistration = new UserRegistration();
-    boolean fName = userRegistration.validateFirstName("Shraddha");
+    boolean fName = userRegistration.validateFirstName("Jayashri");
     Assert.assertTrue(fName);
 }
 
     @Test
     public void givenLastName_WhenProper_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean lName = userRegistration.validateLastName("sarode");
+        boolean lName = userRegistration.validateLastName("Vadde");
         Assert.assertTrue(lName);
     }
     @Test
