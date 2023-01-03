a028895148dcf2fb542a52295be7a47073b966e7
jayashri23 <jsjankar23@gmail.com>
2023-01-03 21:24:49 +0530

    First Name Validation

---
+++ src/com/junit/UserRegistrationTest.java
@@ -0,0 +1,13 @@
+package com.junit;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class UserRegistrationTest {
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean fName = userRegistration.validateFirstName("Jayashri");
+        Assert.assertTrue(fName);
+    }
+}
