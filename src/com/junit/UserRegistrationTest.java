12cb398d50545f9d595ee345f9885763a5dc2c3b
jayashri23 <jsjankar23@gmail.com>
2023-01-03 21:34:50 +0530

    Validation for Email

--- src/com/junit/UserRegistrationTest.java
+++ src/com/junit/UserRegistrationTest.java
@@ -7,14 +7,20 @@ public class UserRegistrationTest {
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean fName = userRegistration.validateFirstName("Jayashri");
+        boolean fName = userRegistration.validateFirstName("Shraddha");
         Assert.assertTrue(fName);
     }
 
     @Test
     public void givenLastName_WhenProper_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean lName = userRegistration.validateLastName("Vadde");
+        boolean lName = userRegistration.validateLastName("sarode");
         Assert.assertTrue(lName);
+    }
+    @Test
+    public void givenEmail_WhenProper_ShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean email = userRegistration.validateEmail("abc.xyz@bl.co.in");
+        Assert.assertTrue(email);
     }
 }
